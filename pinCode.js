db56c60c7be808803e974c01888ccbbe0d9074f3
Harsh R Jain <rjainharsh@gmail.com>
2021-10-07 17:38:02 +0530

    ADDED: validation to check pin code doesnt end with alphabet

--- pinCode.js
+++ pinCode.js
@@ -14,3 +14,10 @@ pinCodeRegex = RegExp('^[0-9]{6}');
 arguments.forEach(element => {
     console.log(element+": ", pinCodeRegex.test(element));
 });
+
+//uc3
+console.log("======= UC 3 =======")
+pinCodeRegex = RegExp('^[0-9]{6}$');
+arguments.forEach(element => {
+    console.log(element+": ", pinCodeRegex.test(element));
+});
