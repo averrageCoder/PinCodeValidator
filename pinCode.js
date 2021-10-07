58706b5773dd9e9735a977c8544a1314eff70c48
Harsh R Jain <rjainharsh@gmail.com>
2021-10-07 17:31:58 +0530

    ADDED: to make sure pin code starts with digit

--- pinCode.js
+++ pinCode.js
@@ -2,8 +2,15 @@ let arguments = process.argv;
 arguments.splice(0,2);
 
 //uc1
-let pinCodeRegex = RegExp('^[0-9]{6}');
-
+console.log("======= UC 1 =======")
+let pinCodeRegex = RegExp('[0-9]{6}');
 arguments.forEach(element => {
-    console.log(element+": ", pinCodeRegexUC1.test(element));
+    console.log(element+": ", pinCodeRegex.test(element));
+});
+
+//uc2
+console.log("======= UC 2 =======")
+pinCodeRegex = RegExp('^[0-9]{6}');
+arguments.forEach(element => {
+    console.log(element+": ", pinCodeRegex.test(element));
 });
